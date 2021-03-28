1744248246f6f64b1c10442e1f284487cc9a0677
Fernando Stecconi <nandilugio@gmail.com>
2021-03-28 14:23:36 +0200

    exercise 3.1.3

--- code_along.hs
+++ code_along.hs
@@ -8,9 +8,11 @@ t251 =
     cMajor = c 4 qn :=: e 4 qn :=: g 4 qn
   in
     dMinor :+: gMajor :+: cMajor
+
 --m = t251
 
--- Excercise 2.1
+-- Excercise 2.1 ---------------------------------------------------------------
+
 twoFiveOne :: Pitch -> Dur -> Music Pitch
 twoFiveOne pit dur =
   let
@@ -19,9 +21,11 @@ twoFiveOne pit dur =
     five = transpose 7 (note dur pit) :=: transpose 11 (note dur pit) :=: transpose 14 (note dur pit)
   in
     two :+: five :+: one
+
 --m = twoFiveOne (G, 2) qn
 
--- Excercise 2.2
+-- Excercise 2.2 ---------------------------------------------------------------
+
 data BluesPitchClass = Ro | MT | Fo | Fi | MS
 type BluesPitch = (BluesPitchClass, Octave)
 
@@ -49,7 +53,8 @@ bluesMelody =
 
 --m = fromBlues(bluesMelody)
 
--- Exercise 2.5
+-- Exercise 2.5 ----------------------------------------------------------------
+
 transM :: AbsPitch -> Music Pitch -> Music Pitch
 transM tp (Prim (Note d p)) = (Prim (Note d (trans tp p)))
 transM tp (Prim (Rest d)) = (Prim (Rest d))
@@ -58,8 +63,30 @@ transM tp (m1 :=: m2) = (transM tp m1 :=: transM tp m2)
 transM tp (Modify c m) = (Modify c (transM tp m))
 
 music = fromBlues(bluesMelody)
-m = music :+: (transM 3 music)
+--m = music :+: (transM 3 music)
 
+-- Exercise 3.1.3
+
+f3Staccato :: [Music Pitch] -> [Music Pitch]
+f3Staccato notes =
+  let staccateNote (Prim (Note d p)) = note (d/2) p :+: rest (d/2)
+  in map staccateNote notes
+
+c7Notes = [c 4 en, e 4 en, g 4 en, bf 4 en]
+exerciseResult = f3Staccato c7Notes
+
+-- folds haven't been introduced but.. to test ;p
+c7ArpeggioStaccato = foldr (:+:) (rest 0) exerciseResult
+c7Arpeggio         = foldr (:+:) (rest 0) c7Notes
+m = c7Arpeggio :+: c7ArpeggioStaccato
+
+-- TODO:
+-- `f3Staccato` assumes it's 1st arg elements are of one particular case of
+-- `Music Pitch`, a leaf node `(Prim (Note Dur Pitch))`.
+-- - Doesn't GHC check that the rest of the cases aren't being handled (like Elm)?
+-- - Can we restrict to that case the input type? Tried
+--     `f3Staccato :: [Primitive Pitch] -> [Music Pitch]`
+--   and some variants but couldn't make it work.
 
 main =
   playDev 5 m
